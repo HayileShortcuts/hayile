6544a7b09d69972ac06e3d5f76e63a4695205589
Ale Schapira <ale.schapira1@gmail.com>
2022-08-24 22:17:53 +0100

    Añadiendo event click a boton de os

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'title',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title.component.scss']
 })
 export class TitleComponent implements OnInit {
-
+  // Con output creamos el nuevo EventEmitter. De este objeto obtendremos los cambios.
+@Output()
+OsSelected = new EventEmitter<string>();
   constructor() { }
-
+    osSelector(operativeSystem: string){
+      this.OsSelected.emit(operativeSystem)
+      console.log(operativeSystem)
+    }
   ngOnInit(): void {
   }
-
 }
